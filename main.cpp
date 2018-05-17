3ea91fe149e86df51f3cddd1e020ec55958e89a9
Francisco de Paula Dias Neto <franciscodias@eufrates.grad.dcc.ufmg.br>
2018-05-17 17:49:10 -0300

    Mudança de string de binários para bitset

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <bitset>
 #include <fstream>
 #include <functional>
 #include <iostream>
@@ -8,94 +9,84 @@
 #include <utility>
 #include <vector>
 
-/*
-class Register {
-public:
-    std::string name;
-    std::string opcode;
-
-    Register(const char *name, const char *opcode) : name(name), opcode(opcode){   
-    }
-};
-*/
 class NoneOperandInstruction {
 public:
-    std::string opcode;
+    std::bitset<16> opcode;
 
-    NoneOperandInstruction(const char *opcode) 
+    NoneOperandInstruction(const std::bitset<16> opcode) 
         : opcode(opcode) {
     }
 };
 
 class OneOperandInstruction {
 public:
-    std::string opcode;
-    std::string firstOperand;
+    std::bitset<5> opcode;
+    std::bitset<11> firstOperand;
 
-    OneOperandInstruction(const char *opcode) 
+    OneOperandInstruction(const std::bitset<5> opcode) 
         : opcode(opcode) {
     }
 
-    OneOperandInstruction(const char *opcode, const char *operand) : opcode(opcode), 
+    OneOperandInstruction(const std::bitset<5> opcode, const std::bitset<11> operand) : opcode(opcode), 
         firstOperand(operand) {
     }
 };
 
 class TwoOperandInstruction {
 public:
-    std::string opcode;
-    std::string firstOperand;
-    std::string secondOperand;
+    std::bitset<5> opcode;
+    std::bitset<2> firstOperand;
+    std::bitset<9> secondOperand;
 
-    TwoOperandInstruction(const char *opcode) 
+    TwoOperandInstruction(const std::bitset<5> opcode) 
         : opcode(opcode) {
     }
 
-    TwoOperandInstruction(const char *opcode, const char *firstOperand, const char *secondOperand) 
+    TwoOperandInstruction(const std::bitset<5> opcode, const std::bitset<2> firstOperand, const std::bitset<9> secondOperand) 
         : opcode(opcode), firstOperand(firstOperand), secondOperand(secondOperand) {
     }
 };
 
 const std::map<std::string, std::shared_ptr<NoneOperandInstruction>> noneOperandInstructions = {
-    {"return", std::make_shared<NoneOperandInstruction>("1000000000000000")},
-    {"stop", std::make_shared<NoneOperandInstruction>("0000000000000000")}
+    {"return", std::make_shared<NoneOperandInstruction>(std::bitset<16>("1000000000000000"))},
+    {"stop", std::make_shared<NoneOperandInstruction>(std::bitset<16>("0000000000000000"))}
 };
 std::map<std::string, std::shared_ptr<OneOperandInstruction>> oneOperandInstruction = {
-    {"copytop", std::make_shared<OneOperandInstruction>("10110")},
-    {"call", std::make_shared<OneOperandInstruction>("01111")},
-    {"push", std::make_shared<OneOperandInstruction>("01101")},
-    {"pop", std::make_shared<OneOperandInstruction>("01110")},
-    {"read", std::make_shared<OneOperandInstruction>("00011")},
-    {"write", std::make_shared<OneOperandInstruction>("00100")},
-    {"jump", std::make_shared<OneOperandInstruction>("01001")}
+    {"copytop", std::make_shared<OneOperandInstruction>(std::bitset<5>("10110"))},
+    {"call", std::make_shared<OneOperandInstruction>(std::bitset<5>("01111"))},
+    {"push", std::make_shared<OneOperandInstruction>(std::bitset<5>("01101"))},
+    {"pop", std::make_shared<OneOperandInstruction>(std::bitset<5>("01110"))},
+    {"read", std::make_shared<OneOperandInstruction>(std::bitset<5>("00011"))},
+    {"write", std::make_shared<OneOperandInstruction>(std::bitset<5>("00100"))},
+    {"jump", std::make_shared<OneOperandInstruction>(std::bitset<5>("01001"))}
 };
 std::map<std::string, std::shared_ptr<TwoOperandInstruction>> twoOperandInstruction = {
-    {"store_i", std::make_shared<TwoOperandInstruction>("10101")},
-    {"load_i", std::make_shared<TwoOperandInstruction>("10100")},
-    {"load_c", std::make_shared<TwoOperandInstruction>("10011")},
-    {"store_s", std::make_shared<TwoOperandInstruction>("10010")},
-    {"load_s", std::make_shared<TwoOperandInstruction>("10001")},
-    {"move", std::make_shared<TwoOperandInstruction>("01100")},
-    {"divide", std::make_shared<TwoOperandInstruction>("01000")},
-    {"multiply", std::make_shared<TwoOperandInstruction>("00111")},
-    {"subtract", std::make_shared<TwoOperandInstruction>("00110")},
-    {"load", std::make_shared<TwoOperandInstruction>("00001")},
-    {"store", std::make_shared<TwoOperandInstruction>("00010")},
-    {"add", std::make_shared<TwoOperandInstruction>("00101")},
-    {"jmpz", std::make_shared<TwoOperandInstruction>("01010")},
-    {"jmpn", std::make_shared<TwoOperandInstruction>("01011")}
+    {"store_i", std::make_shared<TwoOperandInstruction>(std::bitset<5>("10101"))},
+    {"load_i", std::make_shared<TwoOperandInstruction>(std::bitset<5>("10100"))},
+    {"load_c", std::make_shared<TwoOperandInstruction>(std::bitset<5>("10011"))},
+    {"store_s", std::make_shared<TwoOperandInstruction>(std::bitset<5>("10010"))},
+    {"load_s", std::make_shared<TwoOperandInstruction>(std::bitset<5>("10001"))},
+    {"move", std::make_shared<TwoOperandInstruction>(std::bitset<5>("01100"))},
+    {"divide", std::make_shared<TwoOperandInstruction>(std::bitset<5>("01000"))},
+    {"multiply", std::make_shared<TwoOperandInstruction>(std::bitset<5>("00111"))},
+    {"subtract", std::make_shared<TwoOperandInstruction>(std::bitset<5>("00110"))},
+    {"load", std::make_shared<TwoOperandInstruction>(std::bitset<5>("00001"))},
+    {"store", std::make_shared<TwoOperandInstruction>(std::bitset<5>("00010"))},
+    {"add", std::make_shared<TwoOperandInstruction>(std::bitset<5>("00101"))},
+    {"jmpz", std::make_shared<TwoOperandInstruction>(std::bitset<5>("01010"))},
+    {"jmpn", std::make_shared<TwoOperandInstruction>(std::bitset<5>("01011"))}
 };
 
-std::map<std::string, std::string> addressMap = {
-    {"A0", "0"},
-    {"A1", "1"},
-    {"A2", "2"},
-    {"A3", "3"}
+std::map<std::string, short> addressMap = {
+    {"A0", 0},
+    {"A1", 1},
+    {"A2", 2},
+    {"A3", 3}
 };
 
 int main(int argc, char const *argv[]) {
     if (argc != 2) {
-        std::cerr << "An argument is need." << std::endl;
+        std::cerr << "An argument is needed." << std::endl;
         exit(1);
     }
     std::string filename(argv[1]);
@@ -105,6 +96,19 @@ int main(int argc, char const *argv[]) {
         exit(1);
     }
 
+	for (std::string line; std::getline(file, line);) {
+		std::istringstream lineStream(line);
+
+		std::string instructionCode;
+		lineStream >> instructionCode;
+
+		if (instructionCode.front() == '_') {
+			// addressMap[instrutionCode];
+		}
+	}
+
+	file.clear();
+	file.seekg(0);
     for (std::string line; std::getline(file, line); ) {
         std::istringstream lineStream(line);
 
@@ -118,8 +122,8 @@ int main(int argc, char const *argv[]) {
 
         auto noneInstruction = noneOperandInstructions.find(instructionCode);
         if (noneInstruction == noneOperandInstructions.end()) {
-		    std::string firstOperand;
-		    lineStream >> firstOperand;
+	    std::string firstOperand;
+	    lineStream >> firstOperand;
             
             auto firstOperandAddress = addressMap.find(firstOperand);
             auto oneInstruction = oneOperandInstruction.find(instructionCode);

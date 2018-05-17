e30d9150b88ff14462dd33ee77cdb458cf147a12
Francisco de Paula Dias Neto <franciscodias@araguaia.grad.dcc.ufmg.br>
2018-05-17 08:58:05 -0300

    Leitura de cada linha de instrução estruturada, ainda para ser testada e inserir a parte de .data

--- main.cpp
+++ main.cpp
@@ -30,15 +30,14 @@ public:
 class OneOperandInstruction {
 public:
     std::string opcode;
-    std::string operand;
+    std::string firstOperand;
 
-    OneOperandInstruction(const char *opcode, std::function<void(T)> instructionFunction) 
+    OneOperandInstruction(const char *opcode) 
         : opcode(opcode) {
     }
 
-    OneOperandInstruction(const char *opcode, const char *operand, 
-        std::function<void(T)> instructionFunction) : opcode(opcode), 
-        operand(operand) {
+    OneOperandInstruction(const char *opcode, const char *operand) : opcode(opcode), 
+        firstOperand(operand) {
     }
 };
 
@@ -61,42 +60,42 @@ const std::map<std::string, std::shared_ptr<NoneOperandInstruction>> noneOperand
     {"return", std::make_shared<NoneOperandInstruction>("1000000000000000")},
     {"stop", std::make_shared<NoneOperandInstruction>("0000000000000000")}
 };
-std::map<std::string, std::shared_ptr<OneOperandInstruction<int>>> oneOperandInstruction = {
-    {"copytop", std::make_shared<OneOperandInstruction<int>>("10110")},
-    {"call", std::make_shared<OneOperandInstruction<int>>("01111")},
-    {"push", std::make_shared<OneOperandInstruction<int>>("01101")},
-    {"pop", std::make_shared<OneOperandInstruction<int>>("01110")},
-    {"read", std::make_shared<OneOperandInstruction<int>>("00011")},
-    {"write", std::make_shared<OneOperandInstruction<int>>("00100")},
-    {"jump", std::make_shared<OneOperandInstruction<int>>("01001")}
+std::map<std::string, std::shared_ptr<OneOperandInstruction>> oneOperandInstruction = {
+    {"copytop", std::make_shared<OneOperandInstruction>("10110")},
+    {"call", std::make_shared<OneOperandInstruction>("01111")},
+    {"push", std::make_shared<OneOperandInstruction>("01101")},
+    {"pop", std::make_shared<OneOperandInstruction>("01110")},
+    {"read", std::make_shared<OneOperandInstruction>("00011")},
+    {"write", std::make_shared<OneOperandInstruction>("00100")},
+    {"jump", std::make_shared<OneOperandInstruction>("01001")}
 };
-std::map<std::string, std::shared_ptr<TwoOperandInstruction<int,int>>> twoOperandInstruction = {
-    {"store_i", std::make_shared<TwoOperandInstruction<int,int>>("10101")},
-    {"load_i", std::make_shared<TwoOperandInstruction<int,int>>("10100")},
-    {"load_c", std::make_shared<TwoOperandInstruction<int,int>>("10011")},
-    {"store_s", std::make_shared<TwoOperandInstruction<int,int>>("10010")},
-    {"load_s", std::make_shared<TwoOperandInstruction<int,int>>("10001")},
-    {"move", std::make_shared<TwoOperandInstruction<int,int>>("01100")},
-    {"divide", std::make_shared<TwoOperandInstruction<int,int>>("01000")},
-    {"multiply", std::make_shared<TwoOperandInstruction<int,int>>("00111")},
-    {"subtract", std::make_shared<TwoOperandInstruction<int,int>>("00110")},
-    {"load", std::make_shared<TwoOperandInstruction<int,int>>("00001")},
-    {"store", std::make_shared<TwoOperandInstruction<int,int>>("00010")},
-    {"add", std::make_shared<TwoOperandInstruction<int,int>>("00101")},
-    {"jmpz", std::make_shared<TwoOperandInstruction<int,int>>("01010")},
-    {"jmpn", std::make_shared<TwoOperandInstruction<int,int>>("01011")}
+std::map<std::string, std::shared_ptr<TwoOperandInstruction>> twoOperandInstruction = {
+    {"store_i", std::make_shared<TwoOperandInstruction>("10101")},
+    {"load_i", std::make_shared<TwoOperandInstruction>("10100")},
+    {"load_c", std::make_shared<TwoOperandInstruction>("10011")},
+    {"store_s", std::make_shared<TwoOperandInstruction>("10010")},
+    {"load_s", std::make_shared<TwoOperandInstruction>("10001")},
+    {"move", std::make_shared<TwoOperandInstruction>("01100")},
+    {"divide", std::make_shared<TwoOperandInstruction>("01000")},
+    {"multiply", std::make_shared<TwoOperandInstruction>("00111")},
+    {"subtract", std::make_shared<TwoOperandInstruction>("00110")},
+    {"load", std::make_shared<TwoOperandInstruction>("00001")},
+    {"store", std::make_shared<TwoOperandInstruction>("00010")},
+    {"add", std::make_shared<TwoOperandInstruction>("00101")},
+    {"jmpz", std::make_shared<TwoOperandInstruction>("01010")},
+    {"jmpn", std::make_shared<TwoOperandInstruction>("01011")}
 };
 
 std::map<std::string, std::string> addressMap = {
-    {"A0", 0},
-    {"A1", 1},
-    {"A2", 2},
-    {"A3", 3}
+    {"A0", "0"},
+    {"A1", "1"},
+    {"A2", "2"},
+    {"A3", "3"}
 };
 
 int main(int argc, char const *argv[]) {
     if (argc != 2) {
-        std::cerr << "A argument is need." << std::endl;
+        std::cerr << "An argument is need." << std::endl;
         exit(1);
     }
     std::string filename(argv[1]);
@@ -116,6 +115,29 @@ int main(int argc, char const *argv[]) {
         if (label != addressMap.end()) {
             lineStream >> instructionCode;
         }
+
+        auto noneInstruction = noneOperandInstructions.find(instructionCode);
+        if (noneInstruction == noneOperandInstructions.end()) {
+		    std::string firstOperand;
+		    lineStream >> firstOperand;
+            
+            auto firstOperandAddress = addressMap.find(firstOperand);
+            auto oneInstruction = oneOperandInstruction.find(instructionCode);
+            if (oneInstruction == oneOperandInstruction.end()) {
+                std::string secondOperand;
+                lineStream >> secondOperand;
+
+                auto secondOperandAddress = addressMap.find(secondOperand);
+
+                auto twoInstruction = twoOperandInstruction.find(instructionCode);
+
+                twoInstruction->second->firstOperand = firstOperandAddress->second;
+                twoInstruction->second->secondOperand = secondOperandAddress->second;
+            } else {
+                oneInstruction->second->firstOperand = firstOperandAddress->second;
+            }
+
+        }     
     }
     return 0;
 }

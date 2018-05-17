b65476acae06fcf05bb8b342cea3a2fea0d4d3d4
Francisco Dias <franciscopdneto@gmail.com>
2018-05-17 00:54:33 -0300

    Function deletado, mudanças de implementação.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include <utility>
 #include <vector>
 
-#include "Function.h"
 /*
 class Register {
 public:
@@ -22,85 +21,77 @@ public:
 class NoneOperandInstruction {
 public:
     std::string opcode;
-    std::function<void()> instructionFunction;
 
-    NoneOperandInstruction(const char *opcode, std::function<void()> instructionFunction) 
-        : opcode(opcode), instructionFunction(instructionFunction){
+    NoneOperandInstruction(const char *opcode) 
+        : opcode(opcode) {
     }
 };
 
-template<typename T>
 class OneOperandInstruction {
 public:
     std::string opcode;
     std::string operand;
-    std::function<void(T)> instructionFunction;
-    // std::function<void(int)> instructionFunction;
 
     OneOperandInstruction(const char *opcode, std::function<void(T)> instructionFunction) 
-        : opcode(opcode), instructionFunction(instructionFunction){
+        : opcode(opcode) {
     }
 
     OneOperandInstruction(const char *opcode, const char *operand, 
         std::function<void(T)> instructionFunction) : opcode(opcode), 
-        operand(operand), instructionFunction(instructionFunction) {
+        operand(operand) {
     }
 };
 
-template<typename T, typename U>
 class TwoOperandInstruction {
 public:
     std::string opcode;
     std::string firstOperand;
     std::string secondOperand;
-    std::function<void(T,U)> instructionFunction;
 
-    TwoOperandInstruction(const char *opcode, std::function<void(int, int)> instructionFunction) 
-        : opcode(opcode), instructionFunction(instructionFunction) {
+    TwoOperandInstruction(const char *opcode) 
+        : opcode(opcode) {
     }
 
-    TwoOperandInstruction(const char *opcode, const char *firstOperand, const char *secondOperand, 
-        std::function<void(T,U)> instructionFunction) 
-        : opcode(opcode), firstOperand(firstOperand), secondOperand(secondOperand), 
-        instructionFunction(instructionFunction) {
+    TwoOperandInstruction(const char *opcode, const char *firstOperand, const char *secondOperand) 
+        : opcode(opcode), firstOperand(firstOperand), secondOperand(secondOperand) {
     }
 };
 
 const std::map<std::string, std::shared_ptr<NoneOperandInstruction>> noneOperandInstructions = {
-    {"return", std::make_shared<NoneOperandInstruction>("1000000000000000", &Function::returnFunction)},
-    {"stop", std::make_shared<NoneOperandInstruction>("0000000000000000", &Function::stop)}
+    {"return", std::make_shared<NoneOperandInstruction>("1000000000000000")},
+    {"stop", std::make_shared<NoneOperandInstruction>("0000000000000000")}
 };
-std::multimap<std::string, std::shared_ptr<OneOperandInstruction<int>>> oneOperandInstruction = {
-    {"copytop", std::make_shared<OneOperandInstruction<int>>("10110", &Function::copytop)},
-    {"call", std::make_shared<OneOperandInstruction<int>>("01111", &Function::call)},
-    {"push", std::make_shared<OneOperandInstruction<int>>("01101", &Function::push)},
-    {"pop", std::make_shared<OneOperandInstruction<int>>("01110", &Function::pop)},
-    {"read", std::make_shared<OneOperandInstruction<int>>("00011", &Function::read)},
-    {"write", std::make_shared<OneOperandInstruction<int>>("00100", &Function::write)},
-    {"jump", std::make_shared<OneOperandInstruction<int>>("01001", &Function::jump)}
+std::map<std::string, std::shared_ptr<OneOperandInstruction<int>>> oneOperandInstruction = {
+    {"copytop", std::make_shared<OneOperandInstruction<int>>("10110")},
+    {"call", std::make_shared<OneOperandInstruction<int>>("01111")},
+    {"push", std::make_shared<OneOperandInstruction<int>>("01101")},
+    {"pop", std::make_shared<OneOperandInstruction<int>>("01110")},
+    {"read", std::make_shared<OneOperandInstruction<int>>("00011")},
+    {"write", std::make_shared<OneOperandInstruction<int>>("00100")},
+    {"jump", std::make_shared<OneOperandInstruction<int>>("01001")}
 };
-std::multimap<std::string, std::shared_ptr<TwoOperandInstruction<int,int>>> twoOperandInstruction = {
-    {"store_i", std::make_shared<TwoOperandInstruction<int,int>>("10101", &Function::store_i)},
-    {"load_i", std::make_shared<TwoOperandInstruction<int,int>>("10100", &Function::load_i)},
-    {"load_c", std::make_shared<TwoOperandInstruction<int,int>>("10011", &Function::load_c)},
-    {"store_s", std::make_shared<TwoOperandInstruction<int,int>>("10010", &Function::store_s)},
-    {"load_s", std::make_shared<TwoOperandInstruction<int,int>>("10001", &Function::load_s)},
-    {"move", std::make_shared<TwoOperandInstruction<int,int>>("01100", &Function::move)},
-    {"divide", std::make_shared<TwoOperandInstruction<int,int>>("01000", &Function::divide)},
-    {"multiply", std::make_shared<TwoOperandInstruction<int,int>>("00111", &Function::multiply)},
-    {"subtract", std::make_shared<TwoOperandInstruction<int,int>>("00110", &Function::subtract)},
-    {"load", std::make_shared<TwoOperandInstruction<int,int>>("00001", &Function::load)},
-    {"store", std::make_shared<TwoOperandInstruction<int,int>>("00010", &Function::store)},
-    {"add", std::make_shared<TwoOperandInstruction<int,int>>("00101", &Function::add)},
-    {"jmpz", std::make_shared<TwoOperandInstruction<int,int>>("01010", &Function::jmpz)},
-    {"jmpn", std::make_shared<TwoOperandInstruction<int,int>>("01011", &Function::jmpn)}
+std::map<std::string, std::shared_ptr<TwoOperandInstruction<int,int>>> twoOperandInstruction = {
+    {"store_i", std::make_shared<TwoOperandInstruction<int,int>>("10101")},
+    {"load_i", std::make_shared<TwoOperandInstruction<int,int>>("10100")},
+    {"load_c", std::make_shared<TwoOperandInstruction<int,int>>("10011")},
+    {"store_s", std::make_shared<TwoOperandInstruction<int,int>>("10010")},
+    {"load_s", std::make_shared<TwoOperandInstruction<int,int>>("10001")},
+    {"move", std::make_shared<TwoOperandInstruction<int,int>>("01100")},
+    {"divide", std::make_shared<TwoOperandInstruction<int,int>>("01000")},
+    {"multiply", std::make_shared<TwoOperandInstruction<int,int>>("00111")},
+    {"subtract", std::make_shared<TwoOperandInstruction<int,int>>("00110")},
+    {"load", std::make_shared<TwoOperandInstruction<int,int>>("00001")},
+    {"store", std::make_shared<TwoOperandInstruction<int,int>>("00010")},
+    {"add", std::make_shared<TwoOperandInstruction<int,int>>("00101")},
+    {"jmpz", std::make_shared<TwoOperandInstruction<int,int>>("01010")},
+    {"jmpn", std::make_shared<TwoOperandInstruction<int,int>>("01011")}
 };
 
-const std::map<std::string, std::string> registers = {
-    {"A0", "00"},
-    {"A1", "01"},
-    {"A2", "10"},
-    {"A3", "11"}
+std::map<std::string, std::string> addressMap = {
+    {"A0", 0},
+    {"A1", 1},
+    {"A2", 2},
+    {"A3", 3}
 };
 
 int main(int argc, char const *argv[]) {
@@ -121,7 +112,10 @@ int main(int argc, char const *argv[]) {
         std::string instructionCode;
         lineStream >> instructionCode;
 
-        
+        auto label = addressMap.find(instructionCode);
+        if (label != addressMap.end()) {
+            lineStream >> instructionCode;
+        }
     }
     return 0;
 }
